a090b7cbbe5a30f1e5b260f0deeee537a3ca8357
Hunter Kassow <hunterkassow@protonmail.com>
2022-06-24 22:54:36 -0400

    all tests pass

--- javascript/stack.js
+++ javascript/stack.js
@@ -9,45 +9,58 @@ class Stack {
   // add item to top of stack if not full
   // if full throw error
   push(item) {
-
+    if (this.stack.length < 10) {
+       this.stack.push(item)
+    } else {
+      throw new Error('Stack is full!');
+    }
   }
 
   // remove item from top of stack and return it
   pop() {
-
+    return this.stack.pop();
   }
 
   // return item at top of stack without removing it
   peek() {
-
+    return this.stack[this.size() -1]
   }
 
   // return true if stack is empty, otherwise false
   isEmpty() {
-
+    return this.size()===0
   }
 
   // return true if stack is full, otherwise false
   isFull() {
-
+    return this.size()===10
   }
 
   // return number of items in stack
   size() {
-
+    return this.stack.length
   }
 
   // return -1 if item not in stack, otherwise integer representing 
   // how far it is from the top
   search(target) {
-
+    let result = -1
+    let limit = this.size()
+    for (let i=0;i<limit;i++) {
+      //console.log(this.stack[i])
+      if (target === this.stack[limit -1 - i]){   
+        result = i
+        return result
+      }
+    }
+    return result
+    
   }
 
   // print contents of stack: do not return the stack itself!
   print() {
-    
   }
-}
+} 
 
 if (require.main === module) {
   // add your own tests in here
